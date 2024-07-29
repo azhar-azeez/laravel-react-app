1c3c146d12e9730ae9886025cec0d7e635329ce3
Azhar Azeez <107195591+azhar-azeez@users.noreply.github.com>
2024-07-30 01:19:25 +0530

    Initial commit

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link, Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../contexts/ContextProvider.jsx'
+
+export default function DefaultLayout() {
+  const {user, token} = useStateContext()
+
+  if (!token) {
+    return <Navigate to="/login" />
+  }
+
+  return (
+    <div id = "defaultLayout">
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Users</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>
+            Header
+          </div>
+          <div>
+            User Info
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  )
+}
